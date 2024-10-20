bac41ff28afeeaf8e706450f3be43fc11e183d7d
Abdallah Serghine <abdallah.serghine@olx.pl>
2024-10-20 20:59:49 +0200

    more work

--- blue-green.sh
+++ blue-green.sh
@@ -8,3 +8,16 @@ kubectl apply -f starter/apps/blue-green/green.yml
 
 echo 'green deployment in progress'
 kubectl -n udacity rollout status deployment/green
+
+echo 'checking green service reachable'
+svc_hostname=$(kubectl get services \
+   --namespace udacity \
+    green-svc \
+   --output jsonpath='{.status.loadBalancer.ingress[0].hostname}')
+echo $svc_hostname
+until curl --output /dev/null --silent --head --fail $svc_hostname; do
+    printf 'service no ready yet ..'
+    sleep 5
+done
+
+printf 'service ready!'
