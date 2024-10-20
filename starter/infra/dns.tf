d6b6bc72520e5e5564b8a881b20606caea8fef5b
Abdallah Serghine <abdallah.serghine@olx.pl>
2024-10-20 22:23:18 +0200

    cluster autoscaler

--- starter/infra/dns.tf
+++ starter/infra/dns.tf
@@ -1,16 +1,16 @@
-resource "aws_route53_record" "blue" {
-  zone_id = aws_route53_zone.private_dns.id
-  name    = "blue-green"
-  type    = "CNAME"
-  ttl     = "5"
-
-  weighted_routing_policy {
-    weight = 2
-  }
-
-  set_identifier = "blue"
-  records        = [kubernetes_service.blue.status.0.load_balancer.0.ingress.0.hostname] # https://github.com/hashicorp/terraform-provider-kubernetes/pull/1125
-}
+#resource "aws_route53_record" "blue" {
+#  zone_id = aws_route53_zone.private_dns.id
+#  name    = "blue-green"
+#  type    = "CNAME"
+#  ttl     = "5"
+#
+#  weighted_routing_policy {
+#    weight = 2
+#  }
+#
+#  set_identifier = "blue"
+#  records        = [kubernetes_service.blue.status.0.load_balancer.0.ingress.0.hostname] # https://github.com/hashicorp/terraform-provider-kubernetes/pull/1125
+#}
 
 resource "aws_route53_record" "green" {
   zone_id = aws_route53_zone.private_dns.id
@@ -23,7 +23,7 @@ resource "aws_route53_record" "green" {
   }
 
   set_identifier = "green"
-  records        = ["aae24384eef344cc988948e21bf6931d-423eb1d213cc5520.elb.us-east-2.amazonaws.com"]
+  records        = ["ad1f36148000b402294a3144a937c4e7-2b1d7c4b30e7de8c.elb.us-east-2.amazonaws.com"]  # should create the service using tf so this is not hard coded!!
 }
 
 resource "aws_route53_zone" "private_dns" {
